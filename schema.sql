a2b65a060f74d732ab11473f90d5f0dd2e7efed5
nkatwesigye <nkatwesigye@gmail.com>
2018-06-30 10:24:39 -0700

    Schema update file

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
+## Creates a table with the respective columns that store the Big_IP VIP details 
+## To be run at a MySQL terminal or using a remote schema update script 
 CREATE TABLE `vip_status` (`F5_device_name` varchar(80) DEFAULT NULL,`vip_name` varchar(80) DEFAULT NULL,`vip_ipaddress` varchar(80) NOT NULL,`vip_partition` varchar(80) DEFAULT NULL PRIMARY KEY(vip_ipaddress))
